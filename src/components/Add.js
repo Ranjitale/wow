0a959aea3b4495183c6fe4a7affcb41785413512
Ranjit Ale <120738349+Ranjitale@users.noreply.github.com>
2023-07-13 12:36:31 +0545

    Update Add.js

--- src/components/Add.js
+++ src/components/Add.js
@@ -39,6 +39,7 @@ function Add() {
             <input className=" h-12 border-red-400 mx-36 my-8 w-6/12" type="text" value={title} onChange={(e) => {
               setTitle(e.target.value)
             }} placeholder='Add title..'></input>
+   
             <div className='w-9/12 mx-auto '>
                
                 <form onSubmit={(e) => {
@@ -69,15 +70,8 @@ function Add() {
        </div>
        <div className="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
            <label htmlFor="editor" className="sr-only">Publish post</label>
-                           <pre
-    id="editor"
-    contentEditable="true"
-    onInput={(e) => setContent(e.target.value)}
-    className="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-    placeholder="Write an article..."
-    required
-/>
- </div>
+                            <textarea id="editor" value={content} onChange={(e)=>setContent(e.target.value)} rows="8" className="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write an article..." required></textarea>
+       </div>
    </div>
    <button type="submit" onClick={Add}  className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
        Publish post
