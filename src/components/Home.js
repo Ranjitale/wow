5f1bbe3e3b3787114a6957145742b0f5e9b65334
Ranjitale <ranjitalemagar4@gmail.com>
2023-09-12 08:00:33 +0545

    fresh edit'

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,12 +8,12 @@ const Home = () => {
       <>
 <div className={`${isMenuOpen?"backdrop-blur-sm blur-sm":""}`}>
       
-<div className="relative h-screen overflow-hidden bg-indigo-900">
+<div className="relative h-auto overflow-hidden bg-indigo-900">
     <img src="https://png.pngtree.com/thumb_back/fh260/background/20210902/pngtree-dragon-boat-festival-dumplings-green-cartoon-nature-theme-poster-image_789264.jpg" alt='bg' className="absolute object-cover w-full h-full"/>
     <div className="absolute inset-0 bg-black opacity-25">
     </div>
     
-    <div className="container relative z-10 flex items-center px-6 py-32 mx-auto md:px-12 xl:py-40">
+    <div className="container relative z-10 flex items-center px-6 py-12 mx-auto md:px-12 xl:py-40">
         <div className="relative z-10 flex flex-col items-start lg:w-3/5 xl:w-2/5">
             <span className="font-bold text-pink-500 font-myFont bg-gray-100 p-4 uppercase">
                 SwingToof Amplify Your Potential
